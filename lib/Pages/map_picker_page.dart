b3d48bc2bda4074b08aefd3f3ea114ed8893202f
AgustinusGalihGumilang <agustinus9141@gmail.com>
2026-07-10 07:34:09 +0700

    [FE][AGG]Fix Scale Photo & Add Warning

--- lib/Pages/map_picker_page.dart
+++ lib/Pages/map_picker_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPickerPage extends StatefulWidget {
